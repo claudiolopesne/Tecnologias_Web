724811456198a98ce3393c7e618aecff7ee2af51
Aluno <claudiolopes.sistemas@gmail.com>
2018-11-05 21:31:54 -0200

    3 trimestre

---
+++ 3_trimestre/aula32.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php include ("menu.php"); ?>
+    <h2> Exemplo de site no PHP</h2>
+    <p>
+      Você pode colocar todo seu HTML dentro
+      desse aquivo PHP que vai funcionar da mesma forma,
+    </p>
+    <p>
+      Para acessar seu site, va em
+      http://localhost/tecnologias_web
+    </p>
+
+
+
+ ?>
+
+  </body>
+</html>
---
+++ 3_trimestre/menu.php
@@ -0,0 +1,10 @@
+
+
+
+<ul>
+  <li> <a href="#">Página inicial</a> </li>
+  <li> <a href="#">Sobre</a> </li>
+  <li> <a href="#">Contato</a> </li>
+  <li> <a href="#">Cadastro</a> </li>
+  <li> <a href="#">login</a> </li>
+</ul>
